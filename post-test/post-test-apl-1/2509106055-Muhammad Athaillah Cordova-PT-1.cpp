01b9b2ceb1fa588693d27e6ba66ebae0a5de865b
Cordova88 <athaillah.cordova@gmail.com>
2026-02-27 17:15:04 +0800

    Memperbaiki output

--- post-test/post-test-apl-1/2509106055-Muhammad Athaillah Cordova-PT-1.cpp
+++ post-test/post-test-apl-1/2509106055-Muhammad Athaillah Cordova-PT-1.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 
-string username;
-string password;
+string usn;
+string pw;
 int jam;
 int menit;
 int detik;
@@ -12,34 +11,44 @@ int waktu;
 int percobaan;
 
 int main(){
-    // system("cls");
     percobaan = 3;
     
     while (percobaan > 0){
+        cout << endl;
         cout << "Masukan Username: ";
-        cin >> username;
+        cin >> usn;
         cout << "Masukan Password: ";
-        cin >> password;
+        cin >> pw;
         cout << endl;
 
-        if (username == "Cordova" && password == "055"){
+        if (usn == "Cordova" && pw == "055"){
+            cout << string(30, '=') << endl;
+            cout <<  endl;
             cout << "Login Berhasil!" << endl;
             cout << endl;
+            cout << string(30, '=') << endl;
             break;
         } else {
             percobaan--;
             cout << "Login Gagal! Sisa Percobaan: " << percobaan << endl;
+            cout << string(30, '=') << endl;
             cout << endl;
         }
         if (percobaan == 0){
-            cout << "Percobaan Habis!, Program Otomatis Keluar" << endl;
+            cout << string(35, '^') << endl;
+            cout << "Percobaan Habis!, Program Selesai" << endl;
+            cout << string(35, '=') << endl;
+            cout << endl;
             return 0;
         }
     }
     do{
+        cout << endl;
         cout << "Pilih Menu" << endl;
-        cout << "1. Konversi Waktu" << endl;
-        cout << "2. Keluar" << endl;
+        cout << "___________________" << endl;
+        cout << "|1. Konversi Waktu|" << endl;
+        cout << "|2. Keluar        |" << endl;
+        cout << "===================" << endl;
         cout << endl;
         cout << "Masukan Pilihan: ";
         cin >> pilihan;
@@ -48,11 +57,13 @@ int main(){
         if (pilihan == 1){
             do{
                 cout << endl;
-                cout << "Pilih Konversi Waktu" << endl;
-                cout << "1. Jam -> Menit dan Detik" << endl;
-                cout << "2. Menit -> Jam dan Detik" << endl;
-                cout << "3. Detik -> Jam dan Menit" << endl;
-                cout << "4. Kembali" << endl;
+                cout << " Pilih Konversi Waktu" << endl;
+                cout << "=============================" << endl;
+                cout << "|1. Jam   -> Menit dan Detik|" << endl;
+                cout << "|2. Menit -> Jam dan Detik  |" << endl;
+                cout << "|3. Detik -> Jam dan Menit  |" << endl;
+                cout << "|4. Kembali                 |" << endl;
+                cout << "=============================" << endl;
                 cout << endl;
                 cout << "Masukan Pilihan: ";
                 cin >> waktu;
@@ -65,6 +76,7 @@ int main(){
                     detik = jam * 3600;
                     cout << jam << " Jam = " << menit << " Menit" << endl;
                     cout << jam << " Jam = " << detik << " Detik" << endl;
+                    cout << string(50, '=') << endl;
                 } else if (waktu == 2){
                     cout << "Masukan Menit: ";
                     cin >> menit;
@@ -72,6 +84,7 @@ int main(){
                     detik = menit * 60;
                     cout << menit << " Menit = " << jam << " Jam" << endl;
                     cout << menit << " Menit = " << detik << " Detik" << endl;
+                    cout << string(50, '=') << endl;
                 } else if (waktu == 3){
                     cout << "Masukan Detik: ";
                     cin >> detik;
@@ -79,14 +92,23 @@ int main(){
                     menit = detik / 60;
                     cout << detik << " Detik = " << jam << " Jam" << endl;
                     cout << detik << " Detik = " << menit << " Menit" << endl;
-                } else {
+                    cout << string(50, '=') << endl;
+                } else if (waktu < 0 ||waktu > 4) {
+                    cout << string(30, '+') << endl;
                     cout << "Pilihan Tidak Valid" << endl;
+                    cout << string(30, '+') << endl;
                 }
             } while (waktu != 4);
-        } else {
+        } else if (pilihan < 0 ||pilihan > 2) {
+            cout << string(30, '+') << endl;
             cout << "Pilihan Tidak Valid" << endl;
+            cout << string(30, '+') << endl;
         }
     } while (pilihan != 2);
+    cout << string(30, '^') << endl;
+    cout << "Program Selesai, terima Kasih" << endl;
+    cout << string(30, '=') << endl;
+    cout << endl;
     
     return 0;
 }
